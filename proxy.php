c6a38556df3505335042fecc01e8db6f50659506
Hyunwoo K <kim@hyunwoo.org>
2018-12-18 11:40:29 +0900

    Simplify return code [proxy.php]

--- proxy.php
+++ proxy.php
@@ -1,43 +1,54 @@
 <?
-// if (!isset($_SERVER['HTTP_X_REQUESTED_WITH'])) {
-//     header('HTTP/1.0 403 Forbidden');
-//     die('<meta http-equiv="refresh" content="0;url=/">');
-// }
+if (!isset($_SERVER['HTTP_X_REQUESTED_WITH'])) {
+    header('HTTP/1.0 403 Forbidden');
+    die('<meta http-equiv="refresh" content="0;url=/">');
+}
 date_default_timezone_set('KST');
-$connect = mysqli_connect('localhost:3307', 'sunrin', 'AccountForSunrin', 'sunrin') or die(0);
+$connect = mysqli_connect('localhost:3307', 'sunrin', 'AccountForSunrin', 'sunrin') or exit(false);
 $sever = 'https://hyperwave.hyunwoo.org/';
 
 switch ($_POST['do']) {
     case 'register':
-        if(!$_POST['uId'] || !$_POST['uPw']) die(false);
+        if (!$_POST['uId'] || !$_POST['uPw']) {
+            die(false);
+        }
         $overlap = false;
         $query = "SELECT uId FROM hyperwave_accounts";
         $result = mysqli_query($connect, $query);
         while ($data = mysqli_fetch_array($result)) {
             if ($_POST['uId'] == $data['uId']) {
-                die(false);
+                exit(false);
             }
         }
-
         $query = "INSERT INTO hyperwave_accounts VALUES (NULL, '" . $_POST['uId'] . "', '" . $_POST['uPw'] . "' , '" . date('Y-m-d H:i:s') . "')";
         $result = mysqli_query($connect, $query);
-        die($result);
-        break;
+        exit($result);
 
     case 'signIn':
-        if(!$_POST['uId'] || !$_POST['uPw']) die(false);
+        if (!$_POST['uId'] || !$_POST['uPw']) {
+            exit(false);
+        }
         $query = "SELECT * FROM hyperwave_accounts";
         $result = mysqli_query($connect, $query);
         while ($data = mysqli_fetch_array($result)) {
             if ($_POST['uId'] == $data['uId'] && $_POST['uPw'] == $data['uPw']) {
-                die(true);
+                exit(true);
             }
         }
-        die(false);
-        break;
+        exit(false);
 
-    case 'update':
+    case 'fetchLevel':
+        $query = "SELECT * FROM hyperwave_accounts";
+        $result = mysqli_query($connect, $query);
+        while ($data = mysqli_fetch_array($result)) {
+            if ($_COOKIE['uId'] == $data['uId']) {
+                exit($data['lvl']);
+            }
+        }
+        exit(false);
+    
+    case 'ingame':
 
     default:
-        return false;
+        exit(false);
 }
